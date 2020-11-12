fa0eff797582d9ac24c239fcd2cfcaeb37fbd35a
etous <elbertjose@hotmail.com>
2020-11-12 03:19:10 -0500

    Chage Channel Fix

--- src/ElibomChannelServiceProvider.php
+++ src/ElibomChannelServiceProvider.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Notification;
 use Lotous\Elibom\Client as ElibomClient;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Notifications\NotificationServiceProvider as BaseNotificationServiceProvider;
-use Illuminate\Contracts\Notifications\Dispatcher as DispatcherContract;
-use Illuminate\Contracts\Notifications\Factory as FactoryContract;
 
 class ElibomChannelServiceProvider extends BaseNotificationServiceProvider
 {
@@ -18,21 +16,6 @@ class ElibomChannelServiceProvider extends BaseNotificationServiceProvider
      */
     public function register()
     {
-
-        $this->app->singleton(ChannelManager::class, function ($app) {
-            $this->app['events']->fire('notification.beforeRegister', [$this]);
-
-            return new ChannelManager($app);
-        });
-
-        $this->app->alias(
-            ChannelManager::class, DispatcherContract::class
-        );
-
-        $this->app->alias(
-            ChannelManager::class, FactoryContract::class
-        );
-
         Notification::resolved(function (ChannelManager $service) {
             $service->extend('elibom', function ($app) {
                 return new Channels\ElibomSmsChannel(
