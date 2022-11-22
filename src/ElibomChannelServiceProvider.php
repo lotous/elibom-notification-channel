2e22371bea306721aba9866f63f03b3413c04413
admin <admin@w360.co>
2022-11-22 01:56:54 -0500

    Update Laravel Version

--- src/ElibomChannelServiceProvider.php
+++ src/ElibomChannelServiceProvider.php
@@ -3,9 +3,10 @@
 namespace Lotous\Elibom\Notifications;
 
 use Illuminate\Support\Facades\Notification;
-use Lotous\Elibom\Client as ElibomClient;
+use Lotous\Elibom\Client;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Notifications\NotificationServiceProvider as BaseNotificationServiceProvider;
+use Lotous\Elibom\Notifications\Channels\ElibomSmsChannel;
 
 class ElibomChannelServiceProvider extends BaseNotificationServiceProvider
 {
@@ -16,10 +17,16 @@ class ElibomChannelServiceProvider extends BaseNotificationServiceProvider
      */
     public function register()
     {
+        $this->app->bind(ElibomSmsChannel::class, function ($app) {
+            return new ElibomSmsChannel(
+                $app->make(Client::class),
+            );
+        });
+
         Notification::resolved(function (ChannelManager $service) {
             $service->extend('elibom', function ($app) {
                 return new Channels\ElibomSmsChannel(
-                    $this->app->make(ElibomClient::class)
+                    $this->app->make(Client::class)
                 );
             });
         });
